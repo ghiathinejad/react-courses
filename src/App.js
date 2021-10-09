85e27c5d0489bda62aa2a2175ea86e2bc14fc117
ghiathinejad <m.ghiathinejad@gmail.com>
2021-10-09 22:32:45 +0330

    course8

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*const App = () => {
     return React.createElement('div',{id:'hello',className:'hello-cls'},"hello");
 }*/
-import Product from "./components/Product/Product";
+/*import Product from "./components/Product/Product";
 const App = () => {
     return(
         <div id="title" className="container">
@@ -16,6 +16,28 @@ const App = () => {
     );
 }
 
+export default App;*/
+
+
+import { Component } from "react";
+import Product from "./components/Product/Product";
+
+class App  extends Component {
+    render() { 
+        return(
+            <div id="title" className="container">
+                <h1>Shopping app</h1>
+                <Product name="javascript" price="10$" />
+                <Product name="nodejs" price="20$" />
+                <Product name="reactjs" price="30$">
+                    "discount is 12%"
+                    <p>happy for you</p>
+                </Product>
+            </div>
+        );
+    }
+}
+ 
 export default App;
 
 export const username = "maryam";
