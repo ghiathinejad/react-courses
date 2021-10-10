807e2b37c569c9a036134c1fa6f0e6cbc8dbe5ee
ghiathinejad <m.ghiathinejad@gmail.com>
2021-10-10 22:29:27 +0330

    course11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 /*const App = () => {
     return React.createElement('div',{id:'hello',className:'hello-cls'},"hello");
 }*/
-/*import Product from "./components/Product/Product";
+import { useState } from "react";
+import Product from "./components/Product/Product";
 const App = () => {
+    const [products,setProducts] = useState( [
+            {id:1 , title : 'javascript' , price : '10$'},
+            {id:2 , title : 'nodejs' , price : '20$'},
+            {id:3 , title : 'reactjs' , price : '30$'}
+        ]
+    );
+    const clickHandler = () =>{
+        setProducts([
+            {id:1 , title : 'javascript' , price : '11$'},
+            {id:2 , title : 'nodejs' , price : '22$'},
+            {id:3 , title : 'reactjs' , price : '33$'}
+        ]
+        );
+    }
     return(
         <div id="title" className="container">
             <h1>Shopping app</h1>
-            <Product name="javascript" price="10$" />
-            <Product name="nodejs" price="20$" />
-            <Product name="reactjs" price="30$">
-                "discount is 12%"
-                <p>happy for you</p>
-            </Product>
+            {products.map((pro) => {
+                    return(<Product key={pro.id} name={pro.title} price={pro.price} />)
+                    }
+                )
+            }
+            <button onClick={clickHandler}>change price</button>
         </div>
     );
 }
 
-export default App;*/
+export default App;
 
 
-import { Component } from "react";
+/*import { Component } from "react";
 import Product from "./components/Product/Product";
 
 class App  extends Component {
@@ -54,7 +69,7 @@ class App  extends Component {
     }
 }
  
-export default App;
+export default App;*/
 
 export const username = "maryam";
 export const mail = "m.ghiathinejad@gmail.com";
