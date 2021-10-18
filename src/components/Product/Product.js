8f5884f518e56b8d9d43266764e8808604204ffd
ghiathinejad <m.ghiathinejad@gmail.com>
2021-10-18 20:14:06 +0330

    course23

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,24 @@
 //import './product.css';
+//import { Fragment } from 'react';
+//import React from 'react';
 import styless from './product.module.css';
 
 const Product = (props) => {
     console.log(props);
     return ( 
+/*         <Fragment>
+            <p>product #{props.name} : {props.price} </p> 
+            {props.children}
+        </Fragment> */
+/*         <React.Fragment>
+            <p>product #{props.name} : {props.price} </p> 
+            {props.children}
+        </React.Fragment> */
+/*         <>
+            <p>product #{props.name} : {props.price} </p> 
+            {props.children}
+        </> */
+
         <div /* className="product" */ className={styless.product} onClick={props.click}>
             <p>product #{props.name} : {props.price} </p> 
             {props.children}
