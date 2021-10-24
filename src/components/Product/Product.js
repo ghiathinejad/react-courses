2cd05804922253cec6c0f9004bf6e341412c0060
ghiathinejad <m.ghiathinejad@gmail.com>
2021-10-24 22:40:08 +0330

    course32 dynamic style in react

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,11 +6,13 @@ class Product extends Component {
     render() { 
         return <Fragment>
             <li className={productStyle.product}>
-                <span>{this.props.product.title}</span><span>:</span> <span>{this.props.product.price}</span>
+                <span>{this.props.product.title}</span>
+                <span>:</span>
+                <span>{this.props.product.price}</span>
                 <span className={productStyle.quantity}>{this.props.product.quantity}</span>
                 <input type="text" onChange={this.props.onChangeTitle} value={this.props.product.title} className={productStyle.input} />
                 <button className={`${productStyle.increment} ${productStyle.button}`} onClick={this.props.onIncrement}>+</button>
-                <button className={`${productStyle.decrement} ${productStyle.button}`} onClick={this.props.onDecrement}>
+                <button className={`${this.props.product.quantity < 2 && productStyle.delete } ${productStyle.button}`} onClick={this.props.onDecrement}>
                     {this.props.product.quantity > 1 ? "-" : <FaTrashAlt />}
                 </button>
                 <button className={`${productStyle.delete} ${productStyle.button}`} onClick={this.props.onDelete}>delete</button>
