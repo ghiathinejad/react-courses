ee4054f31aff154aacaf3196a37483fa1b3eb8cb
ghiathinejad <m.ghiathinejad@gmail.com>
2021-12-26 22:19:22 +0330

    course43 - useEffect

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,5 +1,28 @@
 import navBarStyle from "./navBar.module.css";
+import {useEffect} from "react";
 const NavBar = ({totalItem}) => {
+    // cdm + cdu + cwum => useEffect
+
+    //cdm
+    useEffect(()=>{
+       console.log('use effect mount navbar')
+    },[]);
+
+
+    //cdu
+    useEffect(()=>{
+        console.log('use effect update navbar')
+    },[totalItem]);
+
+    //cwum
+    useEffect(()=>{
+        console.log('use effect cwum1 navbar')
+        return()=>{
+            console.log('use effect cwum2 navbar')
+        }
+    },[]);
+
+
     return (<header className={navBarStyle.navBar}>
         <h2>ghiathinejad shopping</h2>
         <span>({totalItem})</span>
